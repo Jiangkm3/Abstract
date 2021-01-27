9408e67ad1ba7d86b6b27a46e42193440cfdd29b
Thomas Wies <wies@cs.nyu.edu>
2021-01-26 23:44:02 -0500

    pass input file as command line argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,22 @@
+import Control.Monad
 import Init
 import AbstractMonad
 import Abstract1
 import Eval
 import Printer
+import System.Console.GetOpt
+import System.Environment
+import System.Exit
+import System.IO
+import Text.Printf
 
-main = do
-  let fileName = "/home/jiangkm3/peppersieve/app/test.c"
+main = getArgs >>= parse 
+
+parse [fileName] = analyze fileName >> exitSuccess
+
+parse _ = die "Usage: apron-bindings-exe <C input file>" 
+
+analyze fileName = do
   iast <- analyzeAST fileName
   let nast = evalProg iast
   initPrinter nast fileName
--- app/test.c
+++ app/test.c
@@ -1,12 +1,9 @@
 int main() {
 	int a;
 	int i;
-	for(i = 0; a < 100; i += 4) {
-		if (a % 2 == 0) {
-			a += 3;
-		} else {
-			a += 1;
-		}
+        a = 0;
+	for(i = 0; i < 100; i += 1) {
+          a += 1;
 	}
 	return 1;
 }
